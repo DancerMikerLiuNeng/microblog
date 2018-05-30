6e7f35c777ee8b6b4e923e32968b2106cdb75aae
sam <359373468@qq.com>
2018-05-30 10:13:22 -0400

    add gratar photo

--- app/auth/views.py
+++ app/auth/views.py
@@ -62,9 +62,11 @@ def confirm(token):
 def before_request():
     if current_user.is_authenticated:
         current_user.ping()
-    if not current_user.is_authenticated \
-        and request.endpoint[:5] != 'auth.':
-        return redirect(url_for('auth.unconfirmed'))
+        if not current_user.confirmed \
+                and request.endpoint \
+                and request.blueprint != 'auth' \
+                and request.endpoint != 'static':
+            return redirect(url_for('auth.unconfirmed'))
 
 @auth.route('/unconfirmed')
 def unconfirmed():
