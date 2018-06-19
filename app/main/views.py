d6cb092c9c21145e12753ccbb226bc1ccb86ede0
sam <359373468@qq.com>
2018-06-19 10:17:09 -0400

    add permalink and user can edit post

--- app/main/views.py
+++ app/main/views.py
@@ -47,7 +47,7 @@ def user(username):
     # return render_template('user.html', user=user, posts=posts)
     page = request.args.get('page', 1, type=int)
     pagination = user.posts.order_by(Post.timestamp.desc()).paginate(
-        page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+        page, per_page=current_app.config['MICROBLOG_POSTS_PER_PAGE'],
         error_out=False)
     posts = pagination.items
     return render_template('user.html', user=user, posts=posts,
@@ -115,3 +115,26 @@ def index():
     posts = pagination.items
     return render_template('index.html', current_time=datetime.utcnow(),
                            form=form, posts=posts, pagination=pagination)
+
+
+
+@main.route('/post/<int:id>')
+def post(id):
+    post = Post.query.get_or_404(id)
+    return render_template('post.html', posts=[post])
+
+@main.route('/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit(id):
+    post = Post.query.get_or_404(id)
+    if current_user != post.author and \
+        not current_user.can(Permission.ADMIN):
+        abort(404)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.body = form.body.data
+        db.session.add(post)
+        flash('The post has been updated.')
+        return redirect(url_for('.post', id=post.id))
+    form.body.data = post.body
+    return render_template('edit_post.html', form=form)
